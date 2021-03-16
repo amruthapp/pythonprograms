49897aec3453e76e02365cf6f5c3e2102252d2c4
amruthapp <79830617+amruthapp@users.noreply.github.com>
2021-03-16 22:42:00 +0530

    Delete o1program13.py

--- o1program13.py
+++
@@ -1,9 +0,0 @@
-n=int(input("enter the number of colours"))
-list=[]
-for i in range(n):
-    a=input("enter the colour:")
-    list.append(a)
-print(list)
-print("enter the color is:",list[0])
-print("enter the color is:",list[-2])
-
