09cd54f94becebce9691ba9e5e9588d736ab015f
amruthapp <79830617+amruthapp@users.noreply.github.com>
2021-03-24 15:36:48 +0530

    Delete storeover.py

--- pythonprogram/storeover.py
+++
@@ -1,9 +0,0 @@
-n=int(input("enter the number of numbers:"))
-list=[]
-for x in range(n):
-  x=int(input("enter the number"))
-  if x<100:
-    list.append(x)
-  else:
-    list. append('over')
-print(list)
